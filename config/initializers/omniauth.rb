69589e4e30be5482106d8aec80f3364f4e5f8df9
Grant Speelman <grant.speelman@unboxedconsulting.com>
2012-07-05 11:22:29 +0200

    added developer omniauth

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -4,5 +4,6 @@ Rails.application.config.middleware.use OmniAuth::Builder do
   provider :twitter, '', ''
   provider :facebook, '', '', :grant_type => 'client_credentials'
   provider :google, '', ''
+  provider :developer if Rails.env.development?
   # use OmniAuth::Strategies::OpenID, OpenID::Store::Filesystem.new(Rails.root.join('tmp'))
 end
